531339a1a6bbcfcdb1e03bdac4642ed783bc403f
GBaykov <gleb.baykov99@gmail.com>
2023-01-22 23:49:10 +0300

    not: not commit

--- src/routes/deviceRouter.ts
+++ src/routes/deviceRouter.ts
@@ -1,12 +1,12 @@
-import express, {Router} from 'express';
+import express, { Router } from "express";
 const router: Router = express.Router();
-import deviceController from '../controllers/deviceController';
+import deviceController from "../controllers/deviceController";
 
-router.get('/', deviceController.getAll)
-router.post('/',deviceController.create)
+router.get("/", deviceController.getAll);
+router.post("/", deviceController.create);
 
-router.get('/:id', deviceController.getOne)
-router.delete('/:id', deviceController.delete)
-router.put('/:id', deviceController.update)
+router.get("/:id", deviceController.getOne);
+router.delete("/:id", deviceController.delete);
+router.put("/:id", deviceController.update);
 
 export default router;
